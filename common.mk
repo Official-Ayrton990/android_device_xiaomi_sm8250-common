baf4d379a316a0ef8de91090f9e04324112dd500
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-01-16 14:37:18 -0600

    sm8250-common: import common.mk
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

---
+++ common.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
