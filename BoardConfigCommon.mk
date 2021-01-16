c9d2bbaed3810d25dfa96baf3d412616d0c68a45
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-01-16 13:11:59 -0600

    sm8250-common: don't build bootloader
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,3 +18,6 @@ TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a75
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
