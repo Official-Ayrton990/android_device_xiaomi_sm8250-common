88b94c577c3cf1fc458887b7c0a36fca30d91e3c
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-01-16 19:31:29 -0600

    sm8250-common: set shipping api level
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- common.mk
+++ common.mk
@@ -15,6 +15,9 @@ $(call inherit-product-if-exists, vendor/xiaomi/sm8250-common/sm8250-common-vend
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# API
+PRODUCT_SHIPPING_API_LEVEL := 29
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
